c05417b28523ed535356d9a8427b82e233e087a5
Tolstykh Maksym <mrymnik3d@gmail.com>
2023-11-02 11:41:56 +0200

    Рефакторинг під ООП

--- PaintProjectTerm/MainWindow.xaml.cs
+++ PaintProjectTerm/MainWindow.xaml.cs
@@ -1,120 +1,146 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PaintProjectTerm
 {
     public partial class MainWindow : Window
     {
-        //Розмір по замовчуванню
-        private int diameter = (int)Sizes.small;
-        //Колір по замовчуванню ЧОРНИЙ
-        private Brush brushColor = Brushes.Black;
+        private PaintBrush paintBrush;
 
-        private bool isPaint = false;
-        private bool isErase = false;
-
-
-        //Color
-
-        //Розмір пензлика
-        private enum Sizes
-        {
-             small=10,
-             medum=15,
-             large=30
-        }
         public MainWindow()
         {
             InitializeComponent();
-        }
-        //Функція яка створює фігуру коло при малюванні
-        private void paintCircle(Brush circleColor, Point position)
-        {
-            Ellipse ellipse = new Ellipse();
-            ellipse.Fill = circleColor;
-            ellipse.Width = diameter;
-            ellipse.Height = diameter;
-            Canvas.SetTop(ellipse, position.Y);
-            Canvas.SetLeft(ellipse, position.X);
-            paintCanvas.Children.Add(ellipse);
-
+            paintBrush = new PaintBrush(paintCanvas);
         }
 
-        //Функція малювання на канвасі
-        private void Canvas_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (isPaint)
-            {
-                Point mousPos = e.GetPosition(paintCanvas);
-                paintCircle(brushColor,mousPos);
-            }
-            if (isErase)
-            {
-                Point mousPos = e.GetPosition(paintCanvas);
-                paintCircle(brushColor, mousPos);
-            }
-        }
-        // Кнопки кольорів
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
-            brushColor = Brushes.Red;
+            paintBrush.SetColor(Brushes.Red);
         }
 
         private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
         {
-            brushColor = Brushes.Blue;
+            paintBrush.SetColor(Brushes.Blue);
         }
 
         private void RadioButton_Checked_2(object sender, RoutedEventArgs e)
         {
-            brushColor = Brushes.Black;
+            paintBrush.SetColor(Brushes.Black);
         }
 
-        //кнопки ромізрів
         private void RadioButton_Checked_3(object sender, RoutedEventArgs e)
         {
-            diameter = (int)Sizes.small;
+            paintBrush.SetSize(PaintBrush.BrushSize.Small);
         }
 
         private void RadioButton_Checked_4(object sender, RoutedEventArgs e)
         {
-            diameter = (int)Sizes.medum;
+            paintBrush.SetSize(PaintBrush.BrushSize.Medium);
         }
 
         private void RadioButton_Checked_5(object sender, RoutedEventArgs e)
         {
-            diameter = (int)Sizes.large;
+            paintBrush.SetSize(PaintBrush.BrushSize.Large);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            int count = paintCanvas.Children.Count;
-            if (count > 0)
-            {
-                paintCanvas.Children.RemoveAt(count -1);
-            }
+            paintBrush.Undo();
         }
 
         private void paintCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            isPaint = true;
+            paintBrush.StartPainting();
         }
 
         private void paintCanvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            isPaint = false;
+            paintBrush.StopPainting();
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            paintBrush.Paint(e.GetPosition(paintCanvas));
+        }
+    }
+
+    public class PaintBrush
+    {
+        private Canvas canvas;
+        private bool isPainting;
+        private Brush color;
+        private BrushSize size;
+
+        public enum BrushSize
+        {
+            Small = 10,
+            Medium = 15,
+            Large = 30
+        }
+
+        public PaintBrush(Canvas canvas)
+        {
+            this.canvas = canvas;
+            this.isPainting = false;
+            this.color = Brushes.Black;
+            this.size = BrushSize.Small;
+        }
+
+        public void StartPainting()
+        {
+            isPainting = true;
+        }
+
+        public void StopPainting()
+        {
+            isPainting = false;
+        }
+
+        public void Paint(Point position)
+        {
+            if (isPainting)
+            {
+                var ellipse = CreateEllipse(position);
+                canvas.Children.Add(ellipse);
+            }
+        }
+
+        public void SetColor(Brush brush)
+        {
+            color = brush;
+        }
+
+        public void SetSize(BrushSize brushSize)
+        {
+            size = brushSize;
+        }
+
+        public void Undo()
+        {
+            int count = canvas.Children.Count;
+            if (count > 0)
+            {
+                canvas.Children.RemoveAt(count - 1);
+            }
+        }
+
+        private Ellipse CreateEllipse(Point position)
+        {
+            var ellipse = new Ellipse
+            {
+                Fill = color,
+                Width = (int)size,
+                Height = (int)size
+            };
+
+            Canvas.SetTop(ellipse, position.Y);
+            Canvas.SetLeft(ellipse, position.X);
+
+            return ellipse;
         }
     }
 }
